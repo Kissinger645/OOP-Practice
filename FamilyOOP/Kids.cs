a5c506aa77868c630c8500c08131d275c5b14b02
Bobby <kissinger645@gmail.com>
2017-01-17 19:02:18 -0500

    testing out gh syncing

---
+++ FamilyOOP/Kids.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Family
+{
+    enum Mood
+    {
+        Normal, Stressed, Awesome
+    }
+
+    class Kids
+    {
+        public string name;
+        public int chores;
+        public Mood currentMood = Mood.Normal;
+        private int stressLevel;
+
+        public Kids(string _name)
+        {
+            name = _name;
+            chores = 0;
+            stressLevel = 2;
+
+        }
+
+        public void TakeNap()
+        {
+            Console.WriteLine($"{name} took a nap, their stress level went down");
+            stressLevel--;
+        }
+
+        public void DoesChore()
+        {
+            stressLevel++;
+            chores++;
+        }
+
+        public void Fights()
+        {
+            stressLevel++;
+        }
+
+        public override string ToString()
+        {
+            return $"{name} : {chores} : {Mood}";
+        }
+
+
+    }
+}
